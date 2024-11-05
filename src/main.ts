04272e8ebd453bdd41df1abd60a019d289800c8c
yingting1018 <yhuan300@ucsc.edu>
2024-11-04 23:13:56 -0800

    l16 dialog

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
 // todo
-alert('you clicked the button!')
+// Create a button element
+const button = document.createElement('button');
+button.innerText = 'Click Me';
+
+// Append the button to the body
+document.body.appendChild(button);
+
+// Add an event listener to the button
+button.addEventListener('click', () => {
+    alert('you clicked the button!');
+});
